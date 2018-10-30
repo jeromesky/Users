38592b450a2f0da4ca4c586c5c19d42cff88ee94
jeromesky <jerome@glopac.com>
2018-10-30 11:54:52 +1100

    js bug fix

---
+++ modules/address.js
@@ -0,0 +1,13 @@
+
+export default class Profile{
+  constructor() {
+    this.profileID= 0;
+    this.userID   = 0;
+    this.password = '';
+    this.userName = '';
+    this.hash     = '';
+    this.salt     = '';
+    this.bounced  = 0;
+  }
+
+}
